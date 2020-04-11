388c9427cdeed149a200489aaecdf66b02dee59a
Sebastian Kürten <sebastian@topobyte.de>
2020-04-11 14:35:51 +0200

    Improve palette test

--- src/main/java/de/topobyte/terminalemulation/tests/TestPalette.java
+++ src/main/java/de/topobyte/terminalemulation/tests/TestPalette.java
@@ -6,13 +6,19 @@ public class TestPalette
 	public static void main(String[] args)
 	{
 		for (int c = 40; c <= 47; c++) {
-			print(c, String.format("test %d\n", c));
-			print(0, " ");
+			print(c, String.format("test %d", c));
+			print(0, "\n");
 		}
 		for (int c = 30; c <= 37; c++) {
-			print(c, String.format("test %d\n", c));
-			print(0, " ");
+			print(c, String.format("test %d", c));
+			print(0, "\n");
 		}
+		for (int c : new int[] { 39, 49 }) {
+			print(c, String.format("test %d", c));
+			print(0, "\n");
+		}
+		System.out.printf("\033[0;1;39;40mtest");
+		print(0, "\n");
 	}
 
 	private static void print(int color, String text)
